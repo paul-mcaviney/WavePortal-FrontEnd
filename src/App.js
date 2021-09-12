00fc7a640bd11344ade752b4a27c21519747a995
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-12 22:50:59 +0000

    Changed some styling

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ export default function App() {
         </div>
 
         <div className="bio">
-        The name's Paul and this is a space online that you can send waves. It might not seem like much but each wave adds one point to my self-esteem. Wow! So be sure to hit that wave button and help me feel good about myself!
+        The name's Paul and this is a space online that you can <span class="blue-word">send me waves</span>. It might not seem like much but each wave adds one point to my self-esteem. Wow! So be sure to hit that wave button and help me feel good about myself!
         </div>
 
         <button className="waveButton" onClick={wave}>
