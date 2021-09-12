5ba7db919ff79845605ded4ef28db426818bcb06
Paul McAviney <87715498+paul-mcaviney@users.noreply.github.com>
2021-09-12 22:40:25 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import * as React from "react";
+import { ethers } from "ethers";
+import './App.css';
+
+export default function App() {
+
+  const wave = () => {
+    
+  }
+  
+  return (
+    <div className="mainContainer">
+
+      <div className="dataContainer">
+        <div className="header">
+        👋 Welcome To The Wave Factory! 👋
+        </div>
+
+        <div className="bio">
+        The name's Paul and this is a space online that you can send waves. It might not seem like much but each wave adds one point to my self-esteem. Wow! So be sure to hit that wave button and help me feel good about myself!
+        </div>
+
+        <button className="waveButton" onClick={wave}>
+          Wave at Me
+        </button>
+      </div>
+    </div>
+  );
+}
